90cd9cbd43e08d2ee188c943b157034d78fded6e
iftekar2 <iftekar.hossain123@gmail.com>
2023-05-24 19:32:27 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//In here I am stying the Mobile Nav
+let mobileNav = document.getElementById("mobileNav");
+
+function showMobileNav() {
+  mobileNav.classList.toggle("mobileNav");
+}
+
+//This section is for Search Section
+let searchButton = document.getElementById("search");
+
+function showSearch() {
+  searchButton.classList.toggle("search");
+}
